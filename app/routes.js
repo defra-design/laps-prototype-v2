a9d098513f79eaf3ebd6910fee3981f044c7713e
roblav <robert.lavender@accenture.com>
2025-07-16 16:26:58 +0100

    Add user types and confirm bank details journey.

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,16 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// router.get('/', (req, res) => {
+//   console.log('home page');
+// })
+
+router.get('/', (req, res) => {
+    req.session.data['user-type']=''
+    res.render('index')
+})
+
+router.post('/', (req, res) => {
+    res.redirect("/laps/home-page")
+})
