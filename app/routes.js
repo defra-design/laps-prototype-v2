bc63977f698264a9e88c78b488447465e1c84198
roblav <robert.lavender@accenture.com>
2025-07-17 04:18:05 +0100

    Update the confirm bank details journey

--- app/routes.js
+++ app/routes.js
@@ -8,15 +8,50 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-// router.get('/', (req, res) => {
-//   console.log('home page');
+// router.get('*', (req, res, next) => {
+//     console.log('session: ', JSON.stringify(req.session.data));
+//     next()
 // })
 
 router.get('/', (req, res) => {
-    req.session.data['user-type']=''
-    res.render('index')
+    // Reset session data
+    req.session.destroy(function(err) {
+        res.render('index');
+    });
+
 })
 
 router.post('/', (req, res) => {
-    res.redirect("/laps/home-page")
+    res.redirect("/laps/home-page");
 })
+
+router.get('/laps/confirm-bank-details', (req, res, next) => {
+
+    // Store the referrer page to manage the page redirect
+    const referrer = req.get('Referrer') || 'unknown';
+    const parts = referrer.trim().split('/');
+    req.session.data['confirm-bank-details-referrer'] = parts[parts.length - 1];
+    
+    // console.log('Referrer:', req.session.data['confirm-bank-details-referrer']);
+  // Pass control to the next matching route
+  next();
+});
+
+router.post('/laps/confirm-bank-details', (req, res) => {
+    res.redirect("/laps/bank-details-confirmed");
+})
+
+router.post('/laps/bank-details-confirmed', (req, res) => {
+
+    switch (req.session.data['confirm-bank-details-referrer']) {
+        case 'home-page':
+            res.redirect("/laps/home-page")
+            break;
+        case 'bank-details':
+            res.redirect("/laps/bank-details")
+            break;
+        default:
+            res.redirect("/laps/home-page")
+    };
+})
+
